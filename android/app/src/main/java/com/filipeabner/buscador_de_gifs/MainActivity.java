081bcdebe9f3f946b6344a06af24154ed5b2fa32
Filipe Abner <filipeabnersantos@gmail.com>
2021-02-04 02:28:58 -0300

    Adicionado os arquivos do projeto

---
+++ android/app/src/main/java/com/filipeabner/buscador_de_gifs/MainActivity.java
@@ -0,0 +1,6 @@
+package com.filipeabner.buscador_de_gifs;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:buscador_de_gifs/ui/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(
+      hintColor: Colors.white,
+    ),
+    home: HomePage(),
+  ));
+}
